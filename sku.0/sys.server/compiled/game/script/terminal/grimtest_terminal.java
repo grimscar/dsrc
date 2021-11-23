9c33403c54f035c64e883f84989106f9fd3d84ba
Joshua Rhodes <grimscar0827@gmail.com>
2021-11-22 21:40:19 -0500

    Made a test thing

---
+++ sku.0/sys.server/compiled/game/script/terminal/grimtest_terminal.java
@@ -0,0 +1,11 @@
+package script.terminal;
+
+import script.*;
+import script.library.city;
+import script.library.money;
+import script.library.sui;
+import script.library.utils;
+
+public class grimtest_terminal extends script.terminal.base.base_terminal
+{
+}
