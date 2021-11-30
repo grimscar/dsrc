9d6899714aa3375a9fc5a86bb1eced152dd42252
Joshua Rhodes <grimscar0827@gmail.com>
2021-11-30 14:18:05 -0500

    l

--- sku.0/sys.server/compiled/game/script/working/vegvisir/vegvisir.java
+++ sku.0/sys.server/compiled/game/script/working/vegvisir/vegvisir.java
@@ -10,85 +10,89 @@ public class vegvisir extends script.base_script
     {
         StringTokenizer st = new java.util.StringTokenizer(text);
         String command = null;
-        if (text.equals("bunker"))
+        if (st.hasMoreTokens())
         {
-            location loc = getLocation(self);
-            obj_id container = create.object("object/building/general/boss_nass_dungeon.iff", loc);
-            attachScript(container, "restoration.quests.character_slot.cave_master");
-        }
-        if(command.equals("yaw"))
-        {
-            obj_id target = getIntendedTarget(self);
-            if (target == null)
+            command = st.nextToken();
+            if (text.equals("bunker"))
             {
-                String tar = st.nextToken();
+                location loc = getLocation(self);
+                obj_id container = create.object("object/building/general/boss_nass_dungeon.iff", loc);
+                attachScript(container, "restoration.quests.character_slot.cave_master");
+            }
+            if(command.equals("yaw"))
+            {
+                obj_id target = getIntendedTarget(self);
+                if (target == null)
+                {
+                    String tar = st.nextToken();
+                    String amnt = st.nextToken();
+                    obj_id targ = utils.stringToObjId(tar);
+                    modifyYaw(targ, Float.parseFloat(amnt));
+                    sendSystemMessageTestingOnly(self, tar +" "+amnt);
+                }
+                String amount = st.nextToken();
+                modifyYaw(target, Float.parseFloat(amount));
+            }
+            if(command.equals("pitch"))
+            {
+                obj_id target = getIntendedTarget(self);
+                if (target == null)
+                {
+                    String tar = st.nextToken();
+                    String amnt = st.nextToken();
+                    obj_id targ = utils.stringToObjId(tar);
+                    modifyPitch(targ, Float.parseFloat(amnt));
+                    sendSystemMessageTestingOnly(self, tar +" "+amnt);
+                }
+                String amount = st.nextToken();
+                modifyPitch(target, Float.parseFloat(amount));
+            }
+            if(command.equals("roll"))
+            {
+                obj_id target = getIntendedTarget(self);
+                if (target == null)
+                {
+                    String tar = st.nextToken();
+                    String amnt = st.nextToken();
+                    obj_id targ = utils.stringToObjId(tar);
+                    modifyRoll(targ, Float.parseFloat(amnt));
+                    sendSystemMessageTestingOnly(self, tar +" "+amnt);
+                }
+                String amount = st.nextToken();
+                sendSystemMessageTestingOnly(self, amount);
+                modifyRoll(target, Float.parseFloat(amount));
+            }
+            if(command.equals("move"))
+            {
+                obj_id target = getIntendedTarget(self);
+                String tar = "";
+                if (target == null)
+                {
+                    tar = st.nextToken();
+                    target = utils.stringToObjId(tar);
+                }
+                String direction = st.nextToken();
                 String amnt = st.nextToken();
-                obj_id targ = utils.stringToObjId(tar);
-                modifyYaw(targ, Float.parseFloat(amnt));
-                sendSystemMessageTestingOnly(self, tar +" "+amnt);
-            }
-            String amount = st.nextToken();
-            modifyYaw(target, Float.parseFloat(amount));
-        }
-        if(command.equals("pitch"))
-        {
-            obj_id target = getIntendedTarget(self);
-            if (target == null)
-            {
-                String tar = st.nextToken();
-                String amnt = st.nextToken();
-                obj_id targ = utils.stringToObjId(tar);
-                modifyPitch(targ, Float.parseFloat(amnt));
-                sendSystemMessageTestingOnly(self, tar +" "+amnt);
-            }
-            String amount = st.nextToken();
-            modifyPitch(target, Float.parseFloat(amount));
-        }
-        if(command.equals("roll"))
-        {
-            obj_id target = getIntendedTarget(self);
-            if (target == null)
-            {
-                String tar = st.nextToken();
-                String amnt = st.nextToken();
-                obj_id targ = utils.stringToObjId(tar);
-                modifyRoll(targ, Float.parseFloat(amnt));
-                sendSystemMessageTestingOnly(self, tar +" "+amnt);
-            }
-            String amount = st.nextToken();
-            sendSystemMessageTestingOnly(self, amount);
-            modifyRoll(target, Float.parseFloat(amount));
-        }
-        if(command.equals("move"))
-        {
-            obj_id target = getIntendedTarget(self);
-            String tar = "";
-            if (target == null)
-            {
-                tar = st.nextToken();
-                target = utils.stringToObjId(tar);
-            }
-            String direction = st.nextToken();
-            String amnt = st.nextToken();
-            //obj_id targ = utils.stringToObjId(tar);
-            float amount = utils.stringToFloat(amnt);
-            location loc = getLocation(target);
-            switch (direction)
-            {
+                //obj_id targ = utils.stringToObjId(tar);
+                float amount = utils.stringToFloat(amnt);
+                location loc = getLocation(target);
+                switch (direction)
+                {
                 case "x":
-                    loc.x += amount;
-                    break;
-                case "y":
-                    loc.y += amount;
-                    break;
-                case "z":
-                    loc.z += amount;
-                    break;
-                default:
-                    break;
+                        loc.x += amount;
+                        break;
+                    case "y":
+                        loc.y += amount;
+                        break;
+                    case "z":
+                        loc.z += amount;
+                        break;
+                    default:
+                        break;
+                }
+                setLocation(target, loc);
+                sendSystemMessageTestingOnly(self, tar +" "+amnt);
             }
-            setLocation(target, loc);
-            sendSystemMessageTestingOnly(self, tar +" "+amnt);
         }
         return SCRIPT_CONTINUE;
     }
