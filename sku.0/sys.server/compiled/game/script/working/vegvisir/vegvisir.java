a7ea3f11f0b65f2e3a8291f052f65de7f8078961
Joshua Rhodes <grimscar0827@gmail.com>
2021-11-30 14:00:47 -0500

    k

--- sku.0/sys.server/compiled/game/script/working/vegvisir/vegvisir.java
+++ sku.0/sys.server/compiled/game/script/working/vegvisir/vegvisir.java
@@ -13,7 +13,81 @@ public class vegvisir extends script.base_script
             location loc = getLocation(self);
             obj_id container = create.object("object/building/general/boss_nass_dungeon.iff", loc);
             attachScript(container, "restoration.quests.character_slot.cave_master");
-         }
+        }
+        if(command.equals("yaw"))
+        {
+            obj_id target = getIntendedTarget(self);
+            if (target == null)
+            {
+                String tar = st.nextToken();
+                String amnt = st.nextToken();
+                obj_id targ = utils.stringToObjId(tar);
+                modifyYaw(targ, Float.parseFloat(amnt));
+                sendSystemMessageTestingOnly(self, tar +" "+amnt);
+            }
+            String amount = st.nextToken();
+            modifyYaw(target, Float.parseFloat(amount));
+        }
+        if(command.equals("pitch"))
+        {
+            obj_id target = getIntendedTarget(self);
+            if (target == null)
+            {
+                String tar = st.nextToken();
+                String amnt = st.nextToken();
+                obj_id targ = utils.stringToObjId(tar);
+                modifyPitch(targ, Float.parseFloat(amnt));
+                sendSystemMessageTestingOnly(self, tar +" "+amnt);
+            }
+            String amount = st.nextToken();
+            modifyPitch(target, Float.parseFloat(amount));
+        }
+        if(command.equals("roll"))
+        {
+            obj_id target = getIntendedTarget(self);
+            if (target == null)
+            {
+                String tar = st.nextToken();
+                String amnt = st.nextToken();
+                obj_id targ = utils.stringToObjId(tar);
+                modifyRoll(targ, Float.parseFloat(amnt));
+                sendSystemMessageTestingOnly(self, tar +" "+amnt);
+            }
+            String amount = st.nextToken();
+            sendSystemMessageTestingOnly(self, amount);
+            modifyRoll(target, Float.parseFloat(amount));
+        }
+        if(command.equals("move"))
+        {
+            obj_id target = getIntendedTarget(self);
+            String tar = "";
+            if (target == null)
+            {
+                tar = st.nextToken();
+                target = utils.stringToObjId(tar);
+            }
+            String direction = st.nextToken();
+            String amnt = st.nextToken();
+            //obj_id targ = utils.stringToObjId(tar);
+            float amount = utils.stringToFloat(amnt);
+            location loc = getLocation(target);
+            switch (direction)
+            {
+                case "x":
+                    loc.x += amount;
+                    break;
+                case "y":
+                    loc.y += amount;
+                    break;
+                case "z":
+                    loc.z += amount;
+                    break;
+                default:
+                    break;
+            }
+            setLocation(target, loc);
+            sendSystemMessageTestingOnly(self, tar +" "+amnt);
+        }
         return SCRIPT_CONTINUE;
     }
 }
