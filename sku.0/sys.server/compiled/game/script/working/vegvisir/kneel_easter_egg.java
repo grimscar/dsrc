6b34ce6939f35fbc75229ee9b802af01a5f12a88
Joshua Rhodes <grimscar0827@gmail.com>
2021-12-01 17:25:17 -0500

    l

--- sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
+++ sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
@@ -1,6 +1,18 @@
 package script.working.vegvisir;
 
-public class vegvisir extends script.base_script
+import script.obj_id;
+
+public class kneel_easter_egg extends script.base_script
 {
+    public int kneelIthorian(obj_id player, obj_id target) throws InterruptedException
+    {
+        int targetSpecies = getSpecies(target);
+        int posture = getPosture(player);
+        if (targetSpecies == SPECIES_ITHORIAN && posture == POSTURE_CROUCHED)
+        {
+            sendSystemMessage(player, "You hear a faint whisper in your mind...", null);
+        }
+        return SCRIPT_CONTINUE;
+    }
     
 }
