19b343eab6c0427e36d1dbbba0680566e6542e27
Joshua Rhodes <grimscar0827@gmail.com>
2021-12-01 18:40:52 -0500

    s

--- sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
+++ sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
@@ -12,11 +12,10 @@ public class kneel_easter_egg extends script.base_script
 
     public int OnChangedPosture(obj_id self, int oldPosture, int newPosture) throws InterruptedException
     {
-        obj_id target = getTarget(target);
-        obj_id playerKneeling = getIntendedTarget(target);
+        obj_id target = getIntendedTarget(self);
         int targetSpecies = getSpecies(target);
 
-        if (newPosture == POSTURE_CROUCHED && oldPosture != POSTURE_CROUCHED && playerKneeling != self && targetSpecies == SPECIES_ITHORIAN)
+        if (newPosture == POSTURE_CROUCHED && oldPosture != POSTURE_CROUCHED && target != self && targetSpecies == SPECIES_ITHORIAN)
         {
             sendSystemMessage(self, WHISPER, null);
             return SCRIPT_OVERRIDE;
