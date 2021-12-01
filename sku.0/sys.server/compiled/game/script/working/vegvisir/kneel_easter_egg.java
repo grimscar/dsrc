eb4a46fd3f4094962ec6fbb9c0c7026c3d369b05
Joshua Rhodes <grimscar0827@gmail.com>
2021-12-01 18:24:58 -0500

    jj

--- sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
+++ sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
@@ -6,8 +6,8 @@ import script.library.posture;
 
 public class kneel_easter_egg extends script.base_script
 {
-    public static final string_id WHISPER = new string_id("mind_whisper", "You hear a faint whisper from Mother Jungle in your mind...");
-    public static final string_id FAILURE = new string_id("failure", "Something is amiss or not an Ithorian/Kneeling/Self");
+    public static final string_id WHISPER = new String("You hear a faint whisper from Mother Jungle in your mind...");
+    public static final string_id FAILURE = new String("Something is amiss or not an Ithorian/Kneeling/Self");
     public kneel_easter_egg()
     {
     }
@@ -16,43 +16,10 @@ public class kneel_easter_egg extends script.base_script
     {
         if (newPosture == POSTURE_CROUCHED && oldPosture != POSTURE_CROUCHED)
         {
-            sendSystemMessage(self, WHISPER);
+            sendSystemMessage(self, WHISPER, null);
             return SCRIPT_DEFAULT;
         }
-        sendSystemMessage(self, FAILURE);
+        sendSystemMessage(self, FAILURE, null);
         return SCRIPT_CONTINUE;
     }
-    /*
-    public int kneel(obj_id player, obj_id target) throws InterruptedException
-    {
-        int targetSpecies = getSpecies(target);
-        int posture = getPosture(player);
-        obj_id playerKneeling = getIntendedTarget(target);
-        if (targetSpecies == SPECIES_ITHORIAN && posture == POSTURE_CROUCHED && playerKneeling != player)
-        {
-            sendSystemMessage(player, WHISPER);
-        }
-        sendSystemMessage(player, FAILURE);
-        return SCRIPT_CONTINUE;
-    }
-    public int kneel(obj_id object) throws InterruptedException
-    {
-        obj_id target = getIntendedTarget(object);
-        obj_id player = getSelf();
-        int targetSpecies = getSpecies(target);
-        //int posture = getPosture(player);
-        //obj_id playerKneeling = getIntendedTarget(target);
-        if (targetSpecies == SPECIES_ITHORIAN)
-        {
-            sendSystemMessage(player, WHISPER);
-        }
-        sendSystemMessage(player, FAILURE);
-        return SCRIPT_CONTINUE;
-    }
-    public int kneel(obj_id object) throws InterruptedException
-    {
-        obj_id player = getSelf();
-        sendSystemMessage(player, WHISPER);
-        return SCRIPT_CONTINUE;
-    }*/
 }
