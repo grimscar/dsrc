8f66e865f0fe3dfa2824ba2a1179c0dc3133dbfa
Joshua Rhodes <grimscar0827@gmail.com>
2021-12-01 17:40:54 -0500

    asdf

--- sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
+++ sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
@@ -1,16 +1,21 @@
 package script.working.vegvisir;
 
 import script.obj_id;
+import script.string_id;
 
 public class kneel_easter_egg extends script.base_script
 {
+    public static final string_id WHISPER = new string_id("mind_whisper", "You hear a faint whisper from Mother Jungle in your mind...");
+    public kneel_easter_egg()
+    {
+    }
     public int kneelIthorian(obj_id player, obj_id target) throws InterruptedException
     {
         int targetSpecies = getSpecies(target);
         int posture = getPosture(player);
         if (targetSpecies == SPECIES_ITHORIAN && posture == POSTURE_CROUCHED)
         {
-            sendSystemMessage(player, "You hear a faint whisper in your mind...", null);
+            sendSystemMessage(player, WHISPER);
         }
         return SCRIPT_CONTINUE;
     }
