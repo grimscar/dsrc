f87f8afaf914d089ac543cad4a2b71963607e9d5
Joshua Rhodes <grimscar0827@gmail.com>
2021-12-02 03:10:20 -0500

    float

--- sku.0/sys.server/compiled/game/script/theme_park/outbreak/survivor_pathing.java
+++ sku.0/sys.server/compiled/game/script/theme_park/outbreak/survivor_pathing.java
@@ -50,13 +50,13 @@ public class survivor_pathing extends script.base_script
     public int OnMovePathComplete(obj_id self) throws InterruptedException
     {
         setMovementRun(self);
-        setBaseRunSpeed(self, (getBaseRunSpeed(self) - 8));
+        setBaseRunSpeed(self, (getBaseRunSpeed(self) - 7.5f));
         return SCRIPT_CONTINUE;
     }
     public int OnMoveMoving(obj_id self) throws InterruptedException
     {
         setMovementRun(self);
-        setBaseRunSpeed(self, (getBaseRunSpeed(self) - 8));
+        setBaseRunSpeed(self, (getBaseRunSpeed(self) - 7.5f));
         return SCRIPT_CONTINUE;
     }
     public int startSurvivorPathing(obj_id self, dictionary params) throws InterruptedException
@@ -78,8 +78,9 @@ public class survivor_pathing extends script.base_script
             CustomerServiceLog("outbreak_themepark", "survivor_pathing.startSurvivorPathing() Mob: " + self + " FAILED TO FIND waypointPathNodeLocations OBJVAR.");
             return SCRIPT_CONTINUE;
         }
+
         setMovementRun(self);
-        setBaseRunSpeed(self, (getBaseRunSpeed(self) - 8));
+        setBaseRunSpeed(self, (getBaseRunSpeed(self) - 7.5f));
         setInvulnerable(self, false);
         utils.setScriptVar(self, ai_lib.SCRIPTVAR_CACHED_PATROL_NAMED_PATH, wayPtLocs);
         utils.setScriptVar(self, ai_lib.SCRIPTVAR_CACHED_PATROL_TYPE, 0);
