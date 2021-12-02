cf796f2f2e9facf3ab1dd225e81128ffd1eb4c1f
Joshua Rhodes <grimscar0827@gmail.com>
2021-12-02 02:22:49 -0500

    s

--- sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
+++ sku.0/sys.server/compiled/game/script/working/vegvisir/kneel_easter_egg.java
@@ -2,20 +2,18 @@ package script.working.vegvisir;
 
 import script.obj_id;
 
-public class kneel_easter_egg extends script.base_script
-{
+public class kneel_easter_egg extends script.base_script {
     public static final String WHISPER = new String("You hear a faint whisper from Mother Jungle in your mind...");
-    public kneel_easter_egg()
-    {
+
+    public kneel_easter_egg() {
     }
 
-    public int OnChangedPosture(obj_id self, int oldPosture, int newPosture) throws InterruptedException
-    {
+    public int OnChangedPosture(obj_id self, int oldPosture, int newPosture) throws InterruptedException {
         obj_id target = getIntendedTarget(self);
         int targetSpecies = getSpecies(target);
 
-        if (newPosture == POSTURE_CROUCHED && oldPosture != POSTURE_CROUCHED && target != self && targetSpecies == SPECIES_ITHORIAN)
-        {
+        if (newPosture == POSTURE_CROUCHED && oldPosture != POSTURE_CROUCHED && target != self
+                && targetSpecies == SPECIES_ITHORIAN) {
             sendSystemMessage(self, WHISPER, null);
             return SCRIPT_OVERRIDE;
         }
